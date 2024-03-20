e52e89ee19717d9d7f265b8764588e7e8ae6bdcc
Gagan Kumar <gagan.kumar@dimension64.com>
2024-03-20 19:35:52 +0530

    Clean Up for 2.0

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const loading = () => {
-    return <div className='h-screen bg-primary/5 text-4xl text-center mt-5'></div>;
-};
-
-export default loading;
--- lib/dateFormatter.ts
+++
@@ -1,7 +0,0 @@
-import { parse, format } from 'date-fns';
-
-export const dateFormatter = (dateString) => {
-    const parsedDate = parse(dateString, "yyyy-MM-dd HH:mm:ss.SSSSSSxxx", new Date());
-    const formattedDate = format(parsedDate, "LLL yyyy");
-    return formattedDate;
-};
