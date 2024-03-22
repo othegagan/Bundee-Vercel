3530bf1daec6d55dc0c308fb61b262d11844238e
Gagan Kumar <gagan.kumar@dimension64.com>
2024-03-22 22:22:38 +0530

    Logo Update

--- components/landing_page/Logo.tsx
+++ components/landing_page/Logo.tsx
@@ -1,6 +1,7 @@
 'use client';
+import useHashIdLocalStorage from '@/hooks/useHashIdLocalStorage';
 import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 interface LogoPros {
     className?: string;
@@ -13,7 +14,17 @@ const Logo = ({ className }: LogoPros) => {
         router.push('/');
     }, []);
 
-    return (
+    const [isInIframe, setIsInIframe] = useState(false);
+    useHashIdLocalStorage('hostid');
+
+    useEffect(() => {
+        const inIframe = window !== window.top;
+        setIsInIframe(inIframe);
+    }, []);
+
+    return isInIframe ? (
+        <img src='https://urusreps.com/wp-content/uploads/2024/02/IMG_5755.jpg' className='h-10 w-20' alt='host_logo' />
+    ) : (
         <>
             <div onClick={handleLogoClick} className={`cursor-pointer ${className}`}>
                 <svg xmlns='http://www.w3.org/2000/svg' className='h-10 w-20' viewBox='0 0 200 103' fill='none'>
