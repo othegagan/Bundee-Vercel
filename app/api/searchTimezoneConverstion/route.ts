b35311a40c0ecabe2ed416d7b7f645a43c03b07f
Gagan Kumar <gagan.kumar@dimension64.com>
2024-04-10 14:10:41 +0530

    Update to handle different time format for backend

--- app/api/searchTimezoneConverstion/route.ts
+++ app/api/searchTimezoneConverstion/route.ts
@@ -1,20 +1,36 @@
-//API  route to convert dates for search API
-
 import { convertToCarTimeZoneISO, getSearchDates } from '@/lib/utils';
 import { NextRequest, NextResponse } from 'next/server';
 
+function splitDateTime(dateTimeString: string) {
+    const [datePart, timePart] = dateTimeString.split('T');
+    const date = datePart;
+    const time = timePart.split('.')[0]; // Removing milliseconds
+
+    return { date, time };
+}
+
 export async function POST(req: NextRequest) {
     try {
-        const { latitude, longitude, zipCode, startDate, startTime, endDate, endTime } = await req.json();
+        const { latitude, longitude, zipCode, startDate, startTime, endDate, endTime, startTS, endTS } = await req.json();
+
         let zoneStartDateTime = '';
         let zoneEndDateTime = '';
 
-        if (zipCode == '') {
+        if (zipCode === '') {
             zoneStartDateTime = getSearchDates(Number(longitude), Number(latitude), startDate, startTime);
             zoneEndDateTime = getSearchDates(Number(longitude), Number(latitude), endDate, endTime);
         } else {
-            zoneStartDateTime = convertToCarTimeZoneISO(startDate, startTime, zipCode);
-            zoneEndDateTime = convertToCarTimeZoneISO(endDate, endTime, zipCode);
+            if (!startTS || !endTS) {
+                zoneStartDateTime = convertToCarTimeZoneISO(startDate, startTime, zipCode);
+                zoneEndDateTime = convertToCarTimeZoneISO(endDate, endTime, zipCode);
+            } else {
+                const start = startTS ? splitDateTime(startTS) : { date: startDate, time: startTime };
+                const end = endTS ? splitDateTime(endTS) : { date: endDate, time: endTime };
+
+                console.log(start, end);
+                zoneStartDateTime = convertToCarTimeZoneISO(start.date, start.time, zipCode);
+                zoneEndDateTime = convertToCarTimeZoneISO(end.date, end.time, zipCode);
+            }
         }
 
         const data = {
