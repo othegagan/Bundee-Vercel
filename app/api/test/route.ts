a6e04bb9a4379abd828d190f44eecd105100de75
Gagan Kumar <gagankumar8711@gmail.com>
2024-05-06 19:00:47 +0530

    Add return type

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,18 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
-    const delayPromise = new Promise(resolve => {
+export async function GET(req: NextRequest): Promise<Response> {
+    const delayPromise = new Promise<Response>(resolve => {
         setTimeout(() => {
-            resolve('Promise resolved after 14 seconds');
-            const response = NextResponse.json(
+            resolve(NextResponse.json(
                 {
                     success: true,
                     message: 'Successfully converted to vehicle specified time zone',
                 },
                 { status: 200 },
-            );
-            response.headers.set('Access-Control-Allow-Origin', '*'); // Adding CORS header
-            return response;
+            ));
         }, 14000);
     });
 
