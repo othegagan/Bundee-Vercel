f0080bf381dc6cebfb9617d95018263d6e912ffb
Gagan Kumar <gagankumar8711@gmail.com>
2024-06-12 11:23:54 +0530

    Add firebase errors map

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -17,3 +17,20 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export default app;
+
+export function getFirebaseErrorMessage(code: string) {
+    const errorMap = {
+        'auth/user-not-found': 'User account not found.',
+        'auth/wrong-password': 'Incorrect password. Try again.',
+        'auth/invalid-email': 'Invalid email address.',
+        'auth/too-many-requests': 'Too many requests. Please try again later.',
+        'auth/user-disabled': 'Account has been disabled.',
+        'auth/missing-password': 'Please enter your password.',
+        'auth/invalid-credential': 'Invalid Credentials. Please try again.',
+        'auth/argument-error': 'Invalid argument. Please check your input and try again.',
+        'auth/invalid-phone-number': 'Invalid phone number. Please enter a valid phone number.',
+        'auth/invalid-login-credentials': 'Invalid Credentials. Please try again.',
+        default: 'An error occurred. Please try again.',
+    };
+    return errorMap[code] || errorMap.default;
+}
