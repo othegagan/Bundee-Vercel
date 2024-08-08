96ad087095c718ab36cab0df381079657ce55827
Gagan Kumar <gagankumar8711@gmail.com>
2024-08-08 19:10:33 +0530

    Add client component

--- app/idscan/page.tsx
+++ app/idscan/page.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { useSearchParams } from 'next/navigation';
 import { useIDVC } from '@/hooks/useIDVC';
 import { useEffect } from 'react';
+import ClientOnly from '@/components/ClientOnly';
 
 export default function LicenseVerification() {
     const searchParams = useSearchParams();
@@ -32,34 +33,36 @@ export default function LicenseVerification() {
     }, [isProcessStarted, loadCssFile, removeCssFile]);
 
     return (
-        <div className='h-[calc(100dvh-10px)] flex flex-col overflow-y-auto items-center py-10 px-4  '>
-            {!isProcessStarted && (
-                <>
-                    <h3>License Verification</h3>
-                    <div className='text-center'>
-                        <p>
-                            You are able to start the validation process. <br /> Grab your license and activate the permissions for the
-                            camera.
-                        </p>
-                    </div>
-                </>
-            )}
+        <ClientOnly>
+            <div className='h-[calc(100dvh-10px)] flex flex-col overflow-y-auto items-center py-10 px-4  '>
+                {!isProcessStarted && (
+                    <>
+                        <h3>License Verification</h3>
+                        <div className='text-center'>
+                            <p>
+                                You are able to start the validation process. <br /> Grab your license and activate the permissions for the
+                                camera.
+                            </p>
+                        </div>
+                    </>
+                )}
 
-            <div id='videoCapturingEl' className='w-full max-w-[500px] mt-6' />
+                <div id='videoCapturingEl' className='w-full max-w-[500px] mt-6' />
 
-            {!isProcessStarted && !isLoading && (
-                <Button variant='black' type='button' onClick={startIDVCProcess} disabled={isLoading}>
-                    Start License Validation
-                </Button>
-            )}
-            {isLoading && <div>Loading...</div>}
-            {error && <div>{error}</div>}
+                {!isProcessStarted && !isLoading && (
+                    <Button variant='black' type='button' onClick={startIDVCProcess} disabled={isLoading}>
+                        Start License Validation
+                    </Button>
+                )}
+                {isLoading && <div>Loading...</div>}
+                {error && <div>{error}</div>}
 
-            {/* {isProcessStarted && !isLoading && (
+                {/* {isProcessStarted && !isLoading && (
                 <Button variant='secondary' type='button' onClick={resetProcess}>
                     Reset Process
                 </Button>
             )} */}
-        </div>
+            </div>
+        </ClientOnly>
     );
 }
