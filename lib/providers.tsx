5279b9dd8ed72e808dd58deb705a78a1653f73b9
Gagan Kumar <gagankumar8711@gmail.com>
2024-09-26 12:18:22 +0530

    chore: Add ReactQueryDevtools

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     const queryClient = new QueryClient({
         defaultOptions: {
             queries: {
                 refetchOnWindowFocus: true,
-                staleTime: 2 * 60 * 1000, // Data is fresh for 2 minutes
-                gcTime: 2 * 60 * 1000, // Data is garbage collected after 2 minutes
+                staleTime: 1 * 60 * 1000, // Data is fresh for 2 minute
+                gcTime: 1 * 60 * 1000, // Data is garbage collected after 1 minute
                 retry: 2
             }
         }
