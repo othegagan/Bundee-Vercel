362d4adfc72092f6008d04c9009643327db1161a
Gagan Kumar <gagankumar8711@gmail.com>
2024-11-08 11:43:03 +0530

    feat: add note of $400

--- app/(root)/checkout/payment/page.tsx
+++ app/(root)/checkout/payment/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
             <div className='flex flex-col gap-2'>
                 <h2 className='font-bold text-xl'>Payment</h2>
                 <p>Enter your card details below to complete your reservation.</p>
-                <div className='mb-4 text-muted-foreground text-xs'>
+                <div className='mb-2 text-muted-foreground text-xs'>
                     In the event that your reservation is not accepted by the host, any amount charged to your card will be fully refunded.
                 </div>
                 <div className='mb-4 text-muted-foreground text-xs'>
@@ -30,8 +30,11 @@ export default function page() {
             <h2 className='font-bold text-xl'>Payment</h2>
             <p>Enter your card details below to complete your reservation.</p>
 
-            <div className='mb-4 text-muted-foreground text-xs'>
+            <div className='mb-2 text-muted-foreground text-xs'>
                 In the event that your reservation is not accepted by the host, any amount charged to your card will be fully refunded.
+            </div>
+            <div className='mb-4 text-muted-foreground text-xs'>
+                Note: A refunable security deposit of $400 will be authorized on payments done through debit cards.
             </div>
             <StripePaymentComponent />
         </div>
