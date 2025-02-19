d9cbedb7cc55b23ced7dc9824e7cffd786abfebe
othegagan <hellothegagan@gmail.com>
2025-02-19 20:02:24 +0530

    refactor:  improve layout for better readability

--- app/(root)/trips/TripsComponent.tsx
+++ app/(root)/trips/TripsComponent.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button';
 import useScrollToTopOnLoad from '@/hooks/useScrollToTopOnLoad';
 import { cn, formatDateAndTime, getFullAddress, toTitleCase } from '@/lib/utils';
 import { getTrips } from '@/server/tripOperations';
+import type { TripData } from '@/types';
 import { CalendarDays, MapPin } from 'lucide-react';
 import { useQueryState } from 'next-usequerystate';
 import Link from 'next/link';
@@ -88,38 +89,36 @@ const TripsComponent = () => {
     );
 };
 
-function TripCard({ trip }: { trip: any }) {
+function TripCard({ trip }: { trip: TripData }) {
     const router = useRouter();
     const isTabletOrLarger = useMediaQuery({ query: '(min-width: 768px)' });
-    return (
-        <Link
-            key={trip.tripid}
-            href={`/trips/${trip.tripid}/details`}
-            className={cn(
-                'flex gap-2 border-b p-1 hover:cursor-pointer hover:rounded-md hover:bg-neutral-100 md:p-2',
-                isTabletOrLarger && 'flex w-full flex-1 gap-4 md:py-3'
-            )}>
-            <div className={cn('flex-center overflow-hidden rounded-md', isTabletOrLarger ? 'h-28 w-44' : 'size-28 h-16')}>
-                <img
-                    src={trip.vehicleImages[0]?.imagename || '/images/image_not_available.png'}
-                    alt={`${trip.vehmake} ${trip.vehmodel}`}
-                    className='h-full w-full object-cover object-center'
-                />
-            </div>
 
-            <div className={cn('flex flex-1 flex-col gap-2', isTabletOrLarger && 'w-full')}>
-                <div className={cn('flex items-center justify-between', isTabletOrLarger && 'w-full')}>
-                    <div className={cn('truncate font-semibold text-16 md:max-w-sm', isTabletOrLarger && 'max-w-[200px] font-bold text-[18px]')}>
-                        {toTitleCase(`${trip.vehmake} ${trip.vehmodel} ${trip.vehyear}`)}
-                        <div className='font-medium text-14 text-muted-foreground'>{trip?.vehicleNumber}</div>
-                    </div>
-                    <div className={cn('flex-center justify-between gap-2', isTabletOrLarger && 'mt-2 gap-6')}>
-                        <StatusBadge status={trip.status} type='trip' />
-                        {trip.swapDetails && trip.swapDetails.length > 0 && <StatusBadge status={trip.swapDetails[0].statuscode} type='swap' />}
-                    </div>
+    if (isTabletOrLarger)
+        return (
+            <Link
+                key={trip.tripid}
+                href={`/trips/${trip.tripid}/details`}
+                className='flex w-full flex-1 gap-4 border-b p-2 py-3 hover:cursor-pointer hover:rounded-md hover:bg-neutral-100'>
+                <div className='h-28 w-44 flex-center overflow-hidden rounded-md'>
+                    <img
+                        src={trip.vehicleImages[0]?.imagename || '/images/image_not_available.png'}
+                        alt={`${trip.vehmake} ${trip.vehmodel}`}
+                        className='h-full w-full object-cover object-center'
+                    />
                 </div>
 
-                {isTabletOrLarger && (
+                <div className='flex w-full flex-1 flex-col gap-2'>
+                    <div className='flex w-full items-center justify-between'>
+                        <div className='truncate font-bold text-[18px]'>
+                            {toTitleCase(`${trip.vehmake} ${trip.vehmodel} ${trip.vehyear}`)}
+                            <div className='font-medium text-14 text-muted-foreground'>{trip?.vehicleNumber}</div>
+                        </div>
+                        <div className=' mt-2 flex flex-center justify-between gap-6'>
+                            <StatusBadge status={trip.status} type='trip' />
+                            {trip.swapDetails && trip.swapDetails.length > 0 && <StatusBadge status={trip.swapDetails[0].statuscode} type='swap' />}
+                        </div>
+                    </div>
+
                     <div className='flex w-full items-center gap-2'>
                         <CalendarDays className='size-4 ' />
                         <div className='text-14 '>
@@ -127,26 +126,73 @@ function TripCard({ trip }: { trip: any }) {
                             {formatDateAndTime(trip.endtime, trip?.vehzipcode, 'ddd, MMM DD YYYY')}
                         </div>
                     </div>
-                )}
 
-                <div className={cn('flex items-center gap-2 text-14', isTabletOrLarger && 'flex-center justify-start')}>
-                    <MapPin className={cn('size-5 text-muted-foreground', isTabletOrLarger && 'size-4')} />
-                    <p className={cn('truncate', isTabletOrLarger ? 'max-w-[300px] md:max-w-[400px]' : 'max-w-[300px]')}>
-                        {getFullAddress({ tripDetails: trip })}
-                    </p>
+                    <div className='flex flex-center items-center justify-start gap-2 text-14'>
+                        <MapPin className='size-5 text-muted-foreground' />
+                        <p className='max-w-[400px] truncate'>{getFullAddress({ tripDetails: trip })}</p>
+                    </div>
+
+                    <div className='-mt-6 ml-auto flex items-center justify-end gap-10 pl-1.5'>
+                        <Button
+                            onClick={() => router.push(`/trips/${trip.tripid}/details${isTabletOrLarger ? '' : '?messages=true'}`)}
+                            variant='link'
+                            className='flex items-center gap-2 px-0 font-semibold text-secondary-foreground'>
+                            <img src='/icons/chat.svg' alt='chat' width={18} height={18} />
+                            Message Host
+                        </Button>
+                    </div>
                 </div>
+            </Link>
+        );
 
-                <div className={cn('flex items-center justify-between', isTabletOrLarger && '-mt-6 justify-end gap-10 pl-1.5')}>
-                    <Button
-                        onClick={() => router.push(`/trips/${trip.tripid}/details${isTabletOrLarger ? '' : '?messages=true'}`)}
-                        variant='link'
-                        className='flex items-center gap-2 px-0 font-semibold text-secondary-foreground'>
-                        <img src='/icons/chat.svg' alt='chat' width={18} height={18} />
-                        Message Host
-                    </Button>
+    return (
+        <div className='flex w-full flex-col gap-1 text-nowrap border-b py-2.5 '>
+            <div className='flex gap-3'>
+                <Link className='flex w-fit gap-3' href='/trip-details/2434'>
+                    <div className='relative'>
+                        <div className='col-span-2 h-20 w-32 flex-center overflow-hidden rounded-md border'>
+                            <img
+                                src={trip.vehicleImages[0]?.imagename || '/images/image_not_available.png'}
+                                alt={`${trip.vehmake} ${trip.vehmodel}`}
+                                className='h-full w-full object-cover object-center'
+                            />
+                        </div>
+                    </div>
+                    <div className='flex w-fit flex-col '>
+                        <h1 className='max-w-[230px] truncate font-semibold text-lg '>{toTitleCase(`${trip.vehmake} ${trip.vehmodel} ${trip.vehyear}`)}</h1>
+                        <div className='flex-start gap-5 text-md'>
+                            <span className='text-muted-foreground tracking-wider'>{trip?.vehicleNumber}</span>
+                        </div>
+                        <div className=' mt-3 flex flex-wrap gap-2'>
+                            <StatusBadge status={trip.status} type='trip' />
+                            {trip.swapDetails && trip.swapDetails.length > 0 && <StatusBadge status={trip.swapDetails[0].statuscode} type='swap' />}
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <div className='mt-2 flex w-full items-center gap-2 '>
+                <CalendarDays className='h-4 w-4 text-muted-foreground' />
+                <div className=' md:text-sm dark:text-muted-foreground'>
+                    {formatDateAndTime(trip.starttime, trip?.vehzipcode, 'ddd, MMM DD YYYY')} -{' '}
+                    {formatDateAndTime(trip.endtime, trip?.vehzipcode, 'ddd, MMM DD YYYY')}
                 </div>
             </div>
-        </Link>
+
+            <div className=' line-clamp-1 flex w-full items-center gap-2 dark:text-muted-foreground'>
+                <MapPin className='h-4 w-4 text-muted-foreground' />
+                <div className='max-w-[300px] truncate md:text-sm'>{getFullAddress({ tripDetails: trip })}</div>
+            </div>
+
+            <div className='mt-2 ml-auto flex gap-3'>
+                <Button
+                    onClick={() => router.push(`/trips/${trip.tripid}/details${isTabletOrLarger ? '' : '?messages=true'}`)}
+                    variant='link'
+                    className='flex items-center gap-2 px-0 font-semibold text-secondary-foreground'>
+                    <img src='/icons/chat.svg' alt='chat' width={18} height={18} />
+                    Message Host
+                </Button>
+            </div>
+        </div>
     );
 }
 
